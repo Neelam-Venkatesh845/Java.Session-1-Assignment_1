2966e336a83c370886027086faa7be075079b62e
Neelam-Venaktesh <n.venkateshvvv@gmail.com>
2016-09-14 18:21:43 +0530

    Assignment 1 for session 1 GitHub commit
    Assignment 1 for session 1 GitHub commit

---
+++ Assignment1.java
@@ -0,0 +1,12 @@
+
+public class Assignment1 {
+
+	public static void main(String[] args) {
+		int a = 6000;
+		float b = 2045.225f;
+		int sum = a+ (int)  b;
+				System.out.println(sum);
+		
+	}
+
+}
